f8daead9cbcc7f3483720105be605f02fae89057
TAMARA <119131120+Tamara493@users.noreply.github.com>
2024-05-04 21:35:25 +0300

    remove findByDep from repo

--- src/main/java/com/example/A1/Repository/EmployeeRepository.java
+++ src/main/java/com/example/A1/Repository/EmployeeRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Integer> {
-    List<EmployeeEntity> findByDepartmentId(Integer departmentId);
+
 
 }
 
